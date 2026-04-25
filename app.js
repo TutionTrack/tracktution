480de668786ce299dd8eab54a819dda841acfe12
Sujay Krishna <sujaykrishna19@gmail.com>
2026-04-25 15:35:53 +0400

    Catch node errors in app.js and show in browser

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-// This acts as a wrapper so Hostinger can start the app from the root directory
-require('./server/dist/index.js');
+try {
+  require('./server/dist/index.js');
+} catch (e) {
+  const http = require('http');
+  http.createServer((req, res) => {
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.end('Node App Crashed on Startup: \n\n' + e.toString() + '\n\n' + e.stack);
+  }).listen(process.env.PORT || 3000);
+}
